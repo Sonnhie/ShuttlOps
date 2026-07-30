da2891df6c2123b5ec256447ccfcc50bb3e9da3d
sonny <sonnydelrosario819@gmail.com>
2026-07-30 11:27:32 +0800

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShuttlOps.Controllers
+{
+    public class LoginController : Controller
+    {
+        private ILogger<LoginController> _logger;
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/Login")]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Services/ServiceCollectionExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace ShuttlOps.Services
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddMyAppServices(this IServiceCollection services)
+        {
+            //services.AddScoped<AuthenticationServices>();
+            //services.AddScoped<UserManagementService>();
+            //services.AddScoped<AdminManagementService>();
+
+            return services;
+        }
+    }
+}
